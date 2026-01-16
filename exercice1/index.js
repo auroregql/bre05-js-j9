5e73f20aa368acf7dcaf5689dcde4c8319711c55
auroregql <aurore.gicquel.06@gmail.com>
2026-01-16 09:12:31 +0000

    Exercice 1 terminé : Importer et exporter des variables

---
+++ exercice1/index.js
@@ -0,0 +1,10 @@
+// vos imports ici
+
+import {question} from "./variables.js";
+import {answer} from "./variables.js";
+
+window.addEventListener("DOMContentLoaded", () => {
+   // votre code ici
+   console.log(question);
+   console.log(answer);
+});
---
+++ exercice1/variables.js
@@ -0,0 +1,2 @@
+export let question = "Life, the universe and everything in it";
+export const answer = "42";
